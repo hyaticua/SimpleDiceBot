b45a88c470746505ffb66478b6ed4589109b43c0
Corey Wilder <corey.wilder@motionreality.com>
2020-08-19 00:24:41 -0400

    allow user specified lower bound

--- bot.py
+++ bot.py
@@ -22,12 +22,21 @@ async def on_message(message):
     if message.content.startswith(bot_invocation_str):
         tokens = message.content.split(' ')
 
+        if len(tokens) > 2:
+            return
+
         lower_bound = 1
         upper_bound = 100
         if len(tokens) >= 2: 
-            upper_bound = int(tokens[1])
+            roll_tokens = tokens[1].split('-')
+            if len(roll_tokens) == 2:
+                lower_bound = int(roll_tokens[0])
+            upper_bound = int(roll_tokens[-1])
         
         if not upper_bound > 0:
+            return
+
+        if not lower_bound >= 0:
             return
 
         roll = random.randint(lower_bound, upper_bound)
@@ -36,4 +45,4 @@ async def on_message(message):
         await message.channel.send(msg)
 
 
-client.run('secret key')
+client.run('')
